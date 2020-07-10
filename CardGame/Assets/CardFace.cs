16facb0a3ec9490c056d3935bd48460913b6c53e
Micheal Quentin <mquenti3@gmail.com>
2020-07-10 11:57:10 -0500

    Card Scriptable Object
    Creating cards will be much faster with the scriptable object class.

---
+++ CardGame/Assets/CardFace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardFace : MonoBehaviour
+{
+    public Card card;
+    public TextMesh nameText;
+    public TextMesh typeText;
+    public AudioClip cryAudio;
+    public Sprite cardImage;
+    public TextMesh attackText;
+    public TextMesh healthText;
+    public TextMesh energyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = card.name;
+        typeText.text = card.type;
+        cryAudio = card.cry;
+        cardImage = card.image;
+        attackText.text = card.attack.ToString();
+        healthText.text = card.health.ToString();
+        energyText.text = card.energy.ToString();
+    }
+}
---
+++ CardGame/Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string type;
+    public AudioClip cry;
+    public Sprite image;
+    public int attack;
+    public int health;
+    public int energy;
+}
