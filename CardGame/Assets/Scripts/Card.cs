f4f4e9b4726211ab9ddf4b8bb664ba294e8761e6
Micheal Quentin <mquenti3@gmail.com>
2020-07-10 19:43:16 -0500

    Added DexNumbers to Each Card
    So that the cards can later be sorted either alphabetically or numerically.

--- CardGame/Assets/Scripts/Card.cs
+++ CardGame/Assets/Scripts/Card.cs
@@ -8,6 +8,7 @@ public class Card : ScriptableObject
     //public new string name;
     public string type;
     public AudioClip cry;
+    public int dexNumber;
     public Sprite sprite;
     public Sprite background;
     public int attack;
---
+++ CardGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Card> allCards;
+    public List<Card> deck1;
+    public List<Card> deck2;
+    public List<Card> deck3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assemble all possible cards into a list
+        allCards = new List<Card>();
+
+
+        // If no decks have been created, then it's the player's first time playing. Open the deck creation menu.
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
